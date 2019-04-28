529b63f976de6a765fca76fcd65ced84c6e28cb2
Robert Tinn <t-rotinn@microsoft.com>
2019-04-28 20:01:39 +0100

    Refactor create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -1,29 +1,33 @@
+'''usage: create_dataset.py input_path output_file
+Create a dataset of numpy arrays from a folder of images.'''
+
 import glob as glob
 from PIL import Image
 import numpy as np
 import torchvision.transforms as transforms
-from torch.utils.data import Dataset, DataLoader
 import torch
+import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("input_path")
+parser.add_argument("output_file")
 parser.add_argument('--norm_const', type=int, default=0.5)
 parser.add_argument('--img_dim', type=int, default=32)
 opt = parser.parse_args()
 print(opt)
 
-mean = torch.tensor([_NORM_CONST, _NORM_CONST, _NORM_CONST],
+mean = torch.tensor([opt.norm_const, opt.norm_const, opt.norm_const],
                     dtype=torch.float32)
-std = torch.tensor([_NORM_CONST, _NORM_CONST, _NORM_CONST],
+std = torch.tensor([opt.norm_const, opt.norm_const, opt.norm_const],
                    dtype=torch.float32)
-normalize = transforms.Normalize(mean.tolist(), std.tolist())
-resize_crop = transforms.Compose([transforms.Resize(_IMG_DIM),
-                                  transforms.CenterCrop(_IMG_DIM)])
+resize_crop = transforms.Compose([transforms.Resize(opt.img_dim),
+                                  transforms.CenterCrop(opt.img_dim)])
 tensor_norm = transforms.Compose([transforms.ToTensor(),
                                   transforms.Normalize(mean.tolist(),
                                                        std.tolist())])
 
 img_tensors = torch.tensor([])
-for img_path in glob.glob('./dataset/*.jpg'):
+for img_path in glob.glob(opt.input_folder):
     img = resize_crop(Image.open(img_path))
     i_stack = torch.stack([tensor_norm(img),
                            tensor_norm(img.rotate(180)),
@@ -31,4 +35,4 @@ for img_path in glob.glob('./dataset/*.jpg'):
                            tensor_norm(img.transpose(Image.FLIP_LEFT_RIGHT))])
     img_tensors = torch.cat([img_tensors, i_stack])
 
-np.savez('final_dataset.npz', train_arr=img_tensors.numpy())
+np.savez(opt.output_folder, train_arr=img_tensors.numpy())
