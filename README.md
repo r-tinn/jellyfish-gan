23d9a50eeadb97fa2ebb0bd12b4df919a1238cc5
Robert Tinn <t-rotinn@microsoft.com>
2019-04-28 21:07:52 +0100

    Added a first draft of the README for this project

---
+++ README.md
@@ -0,0 +1,54 @@
+## Description
+
+This project seeks to replicate the model from [Unsupervised Representation Learning with Deep Convolutional Generative Adversarial Networks](https://arxiv.org/abs/1511.06434) (2016) and validate its results.
+
+There are several different reasons for my motivation to undertake this project:
+- Firstly, I was keen to implement an academic paper that I enjoyed reading for myself.
+- I wanted to create my own toy dataset using a few simple steps and minimal data pre-processing.
+- Additionally, it interests me to see how machine learning models perform on more messy real-world datasets to see how much their performance drops.
+- This project gives me an excuse to learn a new machine learning framework (PyTorch) which I have little previous experience of.
+
+
+## Dataset:
+
+My quick and simple idea for creating a dataset was to sample the frames of a video of a specific scene/objects. If most of the video frames contain the objects of interest then a model could be expected to learn its representation. I discovered this video of [Jellyfish](https://www.youtube.com/watch?v=SmfbP17xyqQ) and decided it would be interesting to see if a GAN could capture the unqiue shape of a Jellyfish.
+
+
+## Pre-processing
+
+- The video was sampled to produce ~ 10,000 datapoints.
+
+Examples of the screenshots sampled from the video:
+
+<img src="./readme_images/scene_1.jpg" width="250" caption="a">
+<img src="./readme_images/scene_2.jpg" width="250">
+<img src="./readme_images/scene_3.jpg" width="250">
+<img src="./readme_images/scene_4.jpg" width="250">
+
+
+- The dataset was augmented by applying rotation and reflections to each image to produce ~ 40,000 datapoints.
+- The images were cropped to squares and sub-sampled to 32x32 pixels.
+- The mean and standard deviation for each image was normalised to improve training.
+
+The normalisation of the mean and standard deviation for a single image:
+
+<img src="./readme_images/conversion.png" width="300">
+
+
+## Results
+
+<img src="./readme_images/real_jfish.png" width="100">
+<img src="./readme_images/initial_samples.png" width="100">
+<img src="./readme_images/gen_jfish.png" width="100">
+
+(left: ground truth images, centre: images produced by the untrained generator, right: images synthesised after training)
+
+
+## Conclusions
+
+- With some success a DC-GAN can synthesise semi-realistic Jellyfish.
+- The model struggles to capture fine features such as the tentacles but can better model the overall shape of the jellyfish.
+- GANs clearly scale very well with more compute and data. It is a challenge to train them on small datasets such as this one with limited computational complexity (I trained on local machine with CPU).
+- The GAN struggles more on this dataset than other more carefully processed datasets. There are often multiple Jellyfish in the images for this dataset and sometimes they are only half in the image. This is very different to training a GAN on a dataset such as human faces, where all the faces are carefully positioned to sit in the centre of the image so the positions of features such as the eyes and nose are essentially fixed.
+- It is important to acknowledge the amount of data pre-processing required for training deep generative models such as a DC-GAN. The necessary data pre-processing steps to achieve top performance, such as ensuring only one large jellyfish is present in a single image, take significant time to implement and often a lot of manual labour.
+- This was an interesting project to undertake for a few hours :)
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import argparse
 
-from old_create_dataset import JellyfishDataset
+from utils import JellyfishDataset
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_epochs', type=int, default=100)
@@ -13,3 +13,162 @@ parser.add_argument('--channels', type=int, default=3)
 opt = parser.parse_args()
 print(opt)
 
+cuda = True if torch.cuda.is_available() else False
+
+mean = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float32)
+std = torch.tensor([0.5, 0.5, 0.5], dtype=torch.float32)
+unnormalize = transforms.Normalize((-mean / std).tolist(), (1.0 / std).tolist())
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
+
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+
+        self.init_size = opt.img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128*self.init_size**2))
+
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        out = self.l1(z)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        # print('gen', img.size())
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        def discriminator_block(in_filters, out_filters, bn=True):
+            block = [   nn.Conv2d(in_filters, out_filters, 3, 2, 1),
+                        nn.LeakyReLU(0.2, inplace=True),
+                        nn.Dropout2d(0.25)]
+            if bn:
+                block.append(nn.BatchNorm2d(out_filters, 0.8))
+            return block
+
+        self.model = nn.Sequential(
+            *discriminator_block(opt.channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
+        )
+
+        # The height and width of downsampled image
+        ds_size = opt.img_size // 2**4
+        self.adv_layer = nn.Sequential( nn.Linear(128*ds_size**2, 1),
+                                        nn.Sigmoid())
+
+    def forward(self, img):
+        # print('disc', img.size())
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+
+        return validity
+
+# Loss function
+adversarial_loss = torch.nn.BCELoss()
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    adversarial_loss.cuda()
+
+# Initialize weights
+generator.apply(weights_init_normal)
+discriminator.apply(weights_init_normal)
+
+# Configure data loader
+np_arr = np.load('final_dataset.npz')['train_arr']
+jellyfish_dataset = torch.from_numpy(np_arr)
+dataloader = DataLoader(jellyfish_dataset, batch_size=64, shuffle=True)
+print("finished loading")
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+# ----------
+#  Training
+# ----------
+
+for epoch in range(opt.n_epochs):
+    for i, imgs in enumerate(dataloader):
+
+        # Adversarial ground truths
+        valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
+        fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
+
+        # Configure input
+        real_imgs = Variable(imgs.type(Tensor))
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
+
+        # Generate a batch of images
+        gen_imgs = generator(z)
+        # print(real_imgs.size(), gen_imgs.size())
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = adversarial_loss(discriminator(real_imgs), valid)
+        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]" % (epoch, opt.n_epochs, i, len(dataloader),
+                                                            d_loss.item(), g_loss.item()))
+
+        batches_done = epoch * len(dataloader) + i
+        if batches_done % opt.sample_interval == 0:
+            for j in range(10):
+                save_image(unnormalize(gen_imgs.data[j]), 'images/%d-%d.png' % (batches_done, j), nrow=5, normalize=True)
